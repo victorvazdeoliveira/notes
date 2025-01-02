b9776db4750071b3cdc2390a9a382cd89d5a1d97
Victor Vaz de Oliveira <contato@victorvazdeoliveira.com.br>
2025-01-02 16:40:13 -0300

    Funções adicionar nota e listar nota adicionados

--- bin/notes.dart
+++ bin/notes.dart
@@ -1,7 +1,12 @@
 import 'dart:io';
 
 void main() {
+  List<String> notas = <String>[];
   getComando();
+  adicionaNota(notas);
+  adicionaNota(notas);
+  adicionaNota(notas);
+  listarNotas(notas);
 }
 
 String getComando() {
@@ -18,4 +23,28 @@ String getComando() {
   }
 
   return entrada!;
+}
+
+List<String> adicionaNota(List<String> notas) {
+  print('Escreva uma nota:');
+  String? nota = stdin.readLineSync();
+
+  if (nota == null || nota.isEmpty) {
+    print('Não é possível adicionar uma nota vazia');
+    adicionaNota(notas);
+  }
+
+  notas.add(nota!);
+
+  return notas;
+}
+
+void listarNotas(List<String> notas) {
+  if (notas.isEmpty) {
+    print('Não há notas para listar');
+  }
+
+  for (int i = 0; i < notas.length; i++) {
+    print('Nota ${i + 1}: ${notas[i]}');
+  }
 }
